5a29563e7a6de69f120dacb731224d57fbebb71c
Yueh Chou <yueh.k.chou@gmail.com>
2016-12-18 16:50:02 -0500

    feat() Adding Backend server.js code and webpack settings

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const BUILD_DIR = path.resolve(__dirname, 'client/public');
+const APP_DIR = path.resolve(__dirname, 'client');
+
+const config = {
+  entry [
+    './client/index.js'
+  ],
+  output: {
+    path: BUILD_DIR,
+    filename: 'budle.js'
+  },
+  module: {
+    loaders: [
+      {
+        //Here we have configured it to process both .js and .jsx files using the regular expression.
+        test: /\.js$,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'client')
+      }
+    ]
+  }
+};
+
+modefule.exports = config;
